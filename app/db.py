bdb389cb0dda8d50a505d08a6dbd275b70311e3a
Elizabeth Paperno <liza.paperno@gmail.com>
2022-11-12 22:56:37 -0500

    add all necessary funcs to backend --> testing + working

--- app/db.py
+++ app/db.py
@@ -1,45 +1,109 @@
 import sqlite3   #enable control of an sqlite database
 
-def setup():
+def establishConnection():
     DB_FILE="tables.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
     return (c, db)
 
-def createTables(c): 
+def disconnect(db):
+    db.commit() #save changes
+    db.close()  #close database
+
+def createTables(): 
+    # creates user_info and story_info 
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
     c.execute(f"DROP TABLE IF EXISTS user_info")
-    c.execute(f"DROP TABLE IF EXISTS story_info")
+    c.execute(f"DROP TABLE IF EXISTS story_section_info")
     c.execute(f"CREATE TABLE user_info (user_id TEXT PRIMARY KEY, password TEXT)")
-    c.execute(f"CREATE TABLE story_info (story_id int NOT NULL AUTO_INCREMENT, story_title TEXT, users_edited TEXT)")
-   
+    c.execute(f"CREATE TABLE story_section_info (paragraph_id INTEGER PRIMARY KEY AUTOINCREMENT, story_title TEXT, user_id TEXT, story_section TEXT)")
+    disconnect(db)
 
-def addNewUser(c, username, password): 
+def addNewUser(username, password): 
+    # adds new user and password to user_info table
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
     c.execute(f"INSERT INTO user_info VALUES ('{username}', '{password}');")
+    disconnect(db)
 
-def addNewStory(title, init_user):
-    c.execute(f"INSERT INTO story_info VALUES ('{title}', '{init_user}'+ ',')")
-
-'''
-def addToExistingStory(id, title, user):
+def addSectStory(title, init_user, init_part):
+    # adds new story section to table
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
+    c.execute(f"INSERT INTO story_section_info (story_title, user_id, story_section) VALUES ('{title}', '{init_user}', '{init_part}')")
+    disconnect(db)
 
 def getUsers():
-    # returns list of users
-
+    # returns list of existing users
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
+    vals = c.execute(f"SELECT user_id FROM user_info").fetchall()
+    disconnect(db)
+    formatted_users = []
+    for i in range(len(vals)): 
+        formatted_users.append(vals[i][0])
+    return formatted_users
 
 def getStories():
-    # returns distionary story_id (key), story_title (val)
+    # list of story tiltes (make titles unique)
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
+    vals = c.execute("SELECT DISTINCT story_title FROM story_section_info").fetchall()
+    disconnect(db)
+    formatted_stories= []
+    for i in range(len(vals)): 
+        formatted_stories.append(vals[i][0])
+    return formatted_stories
 
-def getAttributedUsers(story_id):   
-    #given a story id get attributed users in list form
+def getAttributedUsers(title):   
+    #given a story title get attributed users in list form
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
+    vals = c.execute(f"SELECT user_id FROM story_section_info WHERE story_title = '{title}'").fetchall()
+    disconnect(db)
+    formatted_users= []
+    for i in range(len(vals)): 
+        formatted_users.append(vals[i][0])
+    return formatted_users
 
-'''
+def viewLastPar(title): 
+    # last paragraph of given story
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
+    par_id = c.execute(f"SELECT MAX(paragraph_id) FROM story_section_info WHERE story_title = '{title}'").fetchall()[0][0]
+    last_par = c.execute(f"SELECT story_section FROM story_section_info WHERE paragraph_id = '{par_id}'").fetchall()[0][0]
+    disconnect(db)
+    return last_par
 
-setup = setup()
-c = setup[0]
-db = setup[1]
-createTables(c)
-addNewUser(c, "epaperno", "hi")
-addNewStory("i slayed", "epaperno")
-addNewStory("ayo", "asun")
-db.commit() #save changes
-db.close()  #close database
+def viewFullStory(title):
+    #full story in lists of sections
+    conn =  establishConnection()
+    c = conn[0]
+    db = conn[1]
+    vals = c.execute(f"SELECT story_section FROM story_section_info WHERE story_title = '{title}' ORDER BY paragraph_id").fetchall()
+    disconnect(db)
+    formatted_text= []
+    for i in range(len(vals)): 
+        formatted_text.append(vals[i][0])
+    return formatted_text
+    
+createTables()
+addNewUser("epaperno", "hi")
+addNewUser("asun", "hi")
+addSectStory("story1", "epaperno", "i am slaying")
+addSectStory("story2", "asun", "ayo")
+addSectStory("story2", "epaperno", "hi")
+print(getUsers())
+print(getStories())
+print(getAttributedUsers("story1"))
+print(getAttributedUsers("story2"))
+print(viewLastPar("story2"))
+print(viewFullStory("story2"))
